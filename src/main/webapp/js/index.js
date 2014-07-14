c1c5d9b96d0fc29540e6973ced4adb1651d95e19
viknesb <viknesb@gmail.com>
2014-07-13 21:20:51 -0600

    Table to list items in cart

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -1,18 +1,55 @@
-var app = angular.module("cartApp",["controllers"]);
+var app = angular.module("cartApp",["ngRoute","controllers"]);
+
+app.config(function($routeProvider) {
+	$routeProvider
+		.when('/', {
+			controller:'CartCtrl',
+			templateUrl:'list.html'
+		})
+		.when('/edit/:itemId', {
+			controller:'EditCtrl',
+			templateUrl:'detail.html'
+		})
+		.when('/new', {
+			controller:'CreateCtrl',
+			templateUrl:'detail.html'
+		})
+		.otherwise({
+			redirectTo:'/'
+		});
+});
 
 // Controllers
 angular.module("controllers",["services"]).
-controller("CartCtrl",["$scope","CartServer", function($scope,CartServer){
-	CartServer.getItem().then(function(data) {
-		$scope.value = data; 
-	});
-}]);
+	controller("CartCtrl",["$scope","CartServer", function($scope,CartServer){
+		CartServer.getAllItems().then(function(data) {
+			$scope.cart = data;
+			var item = {};
+			var items = [];
+			item.id = 1;
+			item.name = "Watch";
+			item.qty = 2;
+			item.price = "20.00";
+			items.push(item);
+			var item2 = {};
+			item2.id = 2;
+			item2.name = "Laptop";
+			item2.qty = 1;
+			item2.price = "1000.00";
+			items.push(item2);
+			$scope.cart = items;
+			
+		});
+	}]).
+	controller("EditCtrl",["$scope","$location","$routeParams","CartServer", function($scope,$location,$routeParams,CartServer){
+		
+	}]);
 
 //Services
 angular.module("services",[]).
 	factory("CartServer",["$http", function($http) {
 		return {
-			getItem : function() {
+			getAllItems : function() {
 				return $http({method:"GET", url:"api/cart/get",
 					cache : false}).
 				then(function(response) {
